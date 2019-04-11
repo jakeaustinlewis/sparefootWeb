fd994148b6b4e254abb6baad22e962bcd401d2bb
Jake Lewis <jakeaustinlewis@gmail.com>
2019-04-11 11:54:07 -0500

    fixed topContainer alignment in mobile view

--- src/components/animals/animals.js
+++ src/components/animals/animals.js
@@ -10,31 +10,28 @@ class Animals extends PureComponent {
 	
 	render() {
 		return (
-			<div className={`bodyContainer ${styles.hi}`}>
+			<div className={`${styles.bodyContainer}`}>
 				<div>
 				<h1>Animal Adoption Center</h1>
 				{this.props.animals.map((animal, index) => (
-					<section key={index} className='card'>
-						<div className='topCardContainer'>
-
-							<div className='row'>
-								<div className='col'>
-									<img className='imgBorder' src={animal.image} />
+					<section key={index} className={`${styles.card}`}>
+							<div className={`${styles.topCardContainer}`}>
+								<div className='row'>
+									<div className='col'>
+										<img className={`${styles.imgBorder}`} src={animal.image} />
+									</div>
+									<div className='col'>
+										<div className={`${styles.petName}`}>{animal.name} </div>
+									</div>
 								</div>
-								<div className='col'>
-									<div className='petName'>{animal.name} </div>
+								<div className={`${styles.adoptButtonContainer}`}>
+									<button>Adopt {animal.name} today for MONEY AMOUNT</button>
 								</div>
 							</div>
-
-							<div className='adoptButtonContainer'>
-								<button>Adopt {animal.name} today for MONEY AMOUNT</button>
-							</div>
+						<div className={`${styles.bottomCardContainer}`}>
+							<div className={`panel borders hello hi`}>{JSON.stringify(animal, null, 2)}</div>
 						</div>
-						<div className='bottomCardContainer'>
-						</div>
-
 					</section>
-						// <div className={`panel borders hello hi`}>{JSON.stringify(animal, null, 2)}</div>
 				))}
 				</div>
 			</div>
