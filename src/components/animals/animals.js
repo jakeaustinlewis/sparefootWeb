22d016187b070c11aabc459730218feb9ef36abf
Jake Lewis <jakeaustinlewis@gmail.com>
2019-04-11 16:37:36 -0500

    added correct age convention

--- src/components/animals/animals.js
+++ src/components/animals/animals.js
@@ -7,8 +7,19 @@ class Animals extends PureComponent {
 	componentDidMount() {
 		this.props.getAnimals();
 	}
+
+	getAge(age) {
+
+	}
 	
 	render() {
+
+		// let time = animal.age;
+		// let years = Math.floor(animal.age);
+		// let months = Math.floor((animal.age * 365) - (Math.floor(animal.age/365))/30);
+
+		// let years = Math.floor(animal.age);
+		// let month = (animal.age - Math.floor)
 		return (
 			<div className={`${styles.bodyContainer}`}>
 				<div>
@@ -18,14 +29,16 @@ class Animals extends PureComponent {
 							<div className={`${styles.topCardContainer}`}>
 								<div className='row'>
 									<div className='col'>
-										<img className={`${styles.imgBorder}`} src={animal.image} />
+										<img className={`${styles.imgBorder}`} src={require(`../../../public/images/${animal.image}`)} />
+										{/* <img className={`${styles.imgBorder}`} src={require() animal.image} /> */}
 									</div>
 									<div className='col'>
-										<h4 className={`${styles.petName}`}>{`${animal.name.charAt(0).toUpperCase()}${animal.name.slice(1)}`}</h4>
-										<p>{animal.gender}</p>
-										<p>{animal.breed}</p>
-										<p>{animal.breed}</p>
-										<p>{animal.age}</p>
+										<div className={styles.petName}>
+											<h4 className={``}>{`${animal.name.charAt(0).toUpperCase()}${animal.name.slice(1)}`}</h4>
+											<p>{animal.gender}</p>
+											<p>{animal.breed}</p>
+											<p>{`${Math.floor(animal.age)} year, ${Math.floor((animal.age - Math.floor(animal.age))*12)}, months`}</p>
+										</div>
 									</div>
 								</div>
 								<div className={`${styles.adoptButtonContainer}`}>
