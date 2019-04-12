af6c3cdb6af7b21f2c8f4259a9f5db3e74d45e41
Jake Lewis <jakeaustinlewis@gmail.com>
2019-04-12 17:32:28 -0500

    wip

--- src/components/animals/animals.js
+++ src/components/animals/animals.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { getAnimals } from '../../store/animals/actions';
 import styles from './animals.module.scss';
 
-
-
 class Animals extends PureComponent {
 	constructor(props) {
     super(props);
@@ -14,7 +12,9 @@ class Animals extends PureComponent {
 			animals: [],
 		};
 		this.handleDropdownClick = this.handleDropdownClick.bind(this);
-  }
+		this.overallRating = this.overallRating.bind(this);
+	}
+
 	componentDidMount() {
 		document.addEventListener('mousedown', this.handleClickaway, false);
 		this.props.getAnimals();
@@ -59,7 +59,27 @@ class Animals extends PureComponent {
 		}
 		return splitStr.join(' ');
 	}
-	
+
+	overallRating(animalRatings) {
+		let ratingSum = 0;
+		animalRatings.forEach(rating => ratingSum += rating.score);
+		let overallRating = ratingSum/(animalRatings.length)
+		return `Overall Rating ${overallRating}`;
+	}
+
+	ratingScore(ratings, filterStr) {
+		let matchingKey;
+
+		for (let i=0; i<ratings.length; i++) {
+			let keys = Object.keys(ratings[i]);
+			matchingKey = keys.indexOf(filterStr) !== -1
+		}
+		console.log('matching key: ', matchingKey);
+		// let myKeys = Object.keys(myObject)
+		// console.log('filtered rating: ', ratings.filter(rating => rating.score === filterStr).score);
+		// return rating.filter(rating => rating.score === filterStr).score;
+	}
+
 	render() {
 		return (
 			<div className={`${styles.bodyContainer}`}>
@@ -114,13 +134,20 @@ class Animals extends PureComponent {
 							</div>
 						<div className={[animal.species === 'cat' ? `${styles.catColor}` : `${styles.dogColor}`, `${styles.bottomCardContainer}`].join(' ')}>
 							<div className={`${styles.petStats}`}>
-								<h5>Overall Rating</h5>
-								<p>GOOD WITH KIDS </p>
-								<p>ENERGY LEVEL</p>
-								<p>HOUSE BROKEN</p>
-								<p>GOOD WITH PETS</p>
+								<h5>{this.overallRating(animal.ratings)}</h5>
+								{/* <h5>{`Overall Rating ${this.overallRating(animal.ratings)}`} </h5> */}
+
+								{/* { animal.ratings.map(rating => (
+									{ rating.
+									}
+								))} */}
+
+								<p>GOOD WITH KIDS {() => this.ratingScore(animal.ratings, 'children')} </p>
+								<p>ENERGY LEVEL {animal.ratings[1].score}</p>
+								<p>HOUSE BROKEN {animal.ratings[2].score}</p>
+								<p>GOOD WITH PETS {animal.ratings[3].score}</p>
 							</div>
-							{/* <div className={`panel borders hello hi`}>{JSON.stringify(animal, null, 2)}</div> */}
+							<div className={`panel borders hello hi`}>{JSON.stringify(animal, null, 2)}</div>
 						</div>
 					</section>
 				))}
